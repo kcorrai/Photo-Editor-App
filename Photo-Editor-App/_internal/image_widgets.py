2d3c9c4aa35d9118278294925f2400ec9ade6da1
kcorrai <yanlizcakaan@gmail.com>
2024-08-21 13:35:39 +0300

    converted to exe file

---
+++ Photo-Editor-App/_internal/image_widgets.py
@@ -0,0 +1,28 @@
+import customtkinter as ctk
+from tkinter import filedialog, Canvas
+from settings import *
+
+class ImageImport(ctk.CTkFrame):
+    def __init__(self, parent, import_func):
+        super().__init__(master=parent)
+        self.grid(column=0, columnspan=2, row=0, sticky='news')
+        self.import_func = import_func
+        
+        ctk.CTkButton(self, text='open image', command=self.open_dialog).pack(expand=True)
+        
+    def open_dialog(self):
+        path = filedialog.askopenfile().name
+        self.import_func(path)
+        
+class ImageOutput(Canvas):
+    def __init__(self, parent, resize_image):
+        super().__init__(master=parent, background=BACKGROUND_COLOR, bd=0, highlightthickness=0, relief='ridge')
+        self.grid(row=0, column=1, sticky='news')
+        self.bind('<Configure>', resize_image)
+        
+class CloseOutput(ctk.CTkButton):
+    def __init__(self, parent, close_func):
+        super().__init__(master=parent, command=close_func, text='x', text_color=WHITE, fg_color='transparent', width=40, height=40, corner_radius=0, hover_color=CLOSE_RED)
+        self.place(relx=0.99, rely=0.01, anchor='ne')
+        
+        
---
+++ Photo-Editor-App/_internal/panels.py
@@ -0,0 +1,116 @@
+import customtkinter as ctk
+from settings import *
+from tkinter import filedialog
+
+class Panel(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master=parent, fg_color=DARK_GREY)
+        self.pack(fill='x', pady=4, ipady=8)
+        
+class SliderPanel(Panel):
+    def __init__(self, parent, text, data_var, min_value, max_value):
+        super().__init__(parent=parent)
+        
+        # layout
+        self.rowconfigure((0,1), weight=1)
+        self.columnconfigure((0,1), weight=1)
+        
+        self.data_var = data_var
+        self.data_var.trace('w', self.update_text)
+        
+        ctk.CTkLabel(self, text=text).grid(column=0, row=0, sticky='w', padx=5)
+        self.num_label = ctk.CTkLabel(self, text=data_var.get())
+        self.num_label.grid(column=1, row=0, sticky='e', padx=5)
+        
+        ctk.CTkSlider(self, fg_color=SLIDER_BG, variable=self.data_var, from_=min_value, to=max_value, command=self.update_text).grid(row=1, column=0, columnspan=2, sticky='ew', pady=5)
+
+    def update_text(self, *args):
+        self.num_label.configure(text= f'{round(self.data_var.get(), 2)}')
+        
+class SegmentedPanel(Panel):
+    def __init__(self, parent, text, data_var, options):
+        super().__init__(parent=parent)
+        
+        ctk.CTkLabel(self, text=text).pack()
+        ctk.CTkSegmentedButton(self, variable=data_var ,values=options).pack(expand=True, fill='both', padx=4, pady=4)
+        
+class SwitchPanel(Panel):
+    def __init__(self, parent, *args):
+        super().__init__(parent=parent)
+        
+        for var, text in args:
+            switch = ctk.CTkSwitch(self, text=text, variable=var, button_color=BLUE, fg_color=SLIDER_BG)
+            switch.pack(side='left', expand=True, fill='both', padx=5, pady=10)
+            
+class FilenamePanel(Panel):
+    def __init__(self, parent, name_string, file_string):
+        super().__init__(parent=parent)
+        
+        self.name_string = name_string
+        self.name_string.trace('w', self.update_text)
+        self.file_string = file_string
+        self.file_string.trace('w', self.update_text)
+        
+        ctk.CTkEntry(self, textvariable=self.name_string).pack(fill='x', padx=10, pady=10)
+        frame = ctk.CTkFrame(self, fg_color='transparent')
+        jpg_check = ctk.CTkCheckBox(frame, text='jpg', variable=self.file_string, command=self.click('jpg'), onvalue='jpg', offvalue='png')
+        png_check = ctk.CTkCheckBox(frame, text='png', variable=self.file_string, command=self.click('png'), onvalue='png', offvalue='jpg')
+        jpg_check.pack(side='left', fill='x', expand=True)
+        png_check.pack(side='left', fill='x', expand=True)
+        frame.pack(expand=True, fill='x', padx=20)
+        
+        self.output = ctk.CTkLabel(self, text='')
+        self.output.pack()
+    
+    def click(self, value):
+        self.file_string.set(value)
+        self.update_text()
+        
+    def update_text(self, *args):
+        if self.name_string.get():
+            text = self.name_string.get().replace(' ','_') + '.' + self.file_string.get()
+            self.output.configure(text=text)
+            
+class FilePathPanel(Panel):
+    def __init__(self, parent, path_string):
+        super().__init__(parent=parent)
+        self.path_string = path_string
+        
+        ctk.CTkButton(self, text='Open Explorer', command=self.open_dialog).pack(pady=5)
+        ctk.CTkEntry(self, textvariable=self.path_string).pack(expand=True, fill='both', padx=5, pady=5)
+        
+    def open_dialog(self):
+        self.path_string.set(filedialog.askdirectory())
+        
+class DropDownPanel(ctk.CTkOptionMenu):
+    def __init__(self, parent, data_var, options):
+        super().__init__(master=parent, values=options, fg_color=DARK_GREY, button_color=DROPDOWN_MAIN_COLOR, button_hover_color=DROPDOWN_HOVER_COLOR, dropdown_fg_color=DROPDOWN_MENU_COLOR, variable=data_var)
+        self.pack(fill='x', pady=4)
+        
+class RevertButton(ctk.CTkButton):
+    def __init__(self, parent, *args):
+        super().__init__(master=parent, text="Revert", command=self.revert)
+        self.pack(side='bottom', pady=10)
+        self.args = args
+        
+    def revert(self):
+        for var, value in self.args:
+            var.set(value)
+            
+class SaveButton(ctk.CTkButton):
+    def __init__(self, parent, export_image, name_string, file_string, path_string):
+        super().__init__(master=parent, text='Save', command=self.save)
+        self.pack(side='bottom', pady=10)
+        
+        self.export_image = export_image
+        self.name_string = name_string
+        self.file_string = file_string
+        self.path_string = path_string
+        
+    def save(self):
+        self.export_image(self.name_string.get(), self.file_string.get(), self.path_string.get())
+        
+            
+            
+            
+        
